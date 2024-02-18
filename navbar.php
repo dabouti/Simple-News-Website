1d60979e81b0e5b9a013c863240b50aa38d0a7fe
andrewjf04 <112509381+andrewjf04@users.noreply.github.com>
2024-02-18 17:06:16 -0600

    Added logout/login option on navbar

--- navbar.php
+++ navbar.php
@@ -3,4 +3,12 @@
     <a href="createpost.php">Create Posts</a>
     <a href="editpost.php">Edit/Delete Posts</a>
     <a href="editcomment.php">Edit/Delete Comments</a>
+    <?php
+        if(!isset($_SESSION['loggedin'])) {
+            echo '<a href=loginpage.php>Login</a>';
+        }
+        else {
+            echo '<a href="logout.php">Logout</a>';
+        }
+    ?>
 </div>
