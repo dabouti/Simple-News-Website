987c5ddf8adfacd51fb69bea3c05f2462067429b
dabouti <d.h.kim@wustl.edu>
2024-02-18 17:21:30 -0600

    Update main.php

--- main.php
+++ main.php
@@ -12,8 +12,27 @@
     <?php
     session_start();
     include('navbar.php');
-    ?>
+    $stmt = $mysqli->prepare("select post_title, post_username, post_date from posts");
+    if (!$stmt) {
+        printf("Query Prep Failed: %s\n", $mysqli->error);
+        exit;
+    }
 
+    $stmt->execute();
+    $stmt->bind_result($post_title, $post_username, $post_date);
+    echo "<ol>\n";
+    while ($stmt->fetch()) {
+        printf(
+            "\t<li>%s %s</li>\n",
+            htmlspecialchars($post_title),
+            htmlspecialchars($post_username),
+            htmlspecialchars($post_date)
+        );
+    }
+    echo "</ol>\n";
+
+    $stmt->close();
+    ?>
 
 </body>
 
