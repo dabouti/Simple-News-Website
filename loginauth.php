1df5b8d080a7b874d3ce3581210f2950ff997e0e
andrewjf04 <112509381+andrewjf04@users.noreply.github.com>
2024-02-18 15:52:09 -0600

    Update loginauth.php

--- loginauth.php
+++ loginauth.php
@@ -1,5 +1,6 @@
 <?php
 require 'database.php';
+session_start();
 
 $username = $_POST['username'];
 $password = $_POST['password'];
@@ -13,6 +14,7 @@ if (!$stmt) {
 $stmt->execute();
 $stmt->bind_result($user, $password);
 if ($stmt->fetch()) {
+    $_SESSION['loggedin'] = true;
     header("Location: main.php");
 } else {
     header("Location: loginpage.php");
