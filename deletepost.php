b36822dff8b16535a87772874b454bac825d9ba1
dabouti <d.h.kim@wustl.edu>
2024-02-19 01:36:53 -0600

    Update deletepost.php

--- deletepost.php
+++ deletepost.php
@@ -2,6 +2,17 @@
 session_start();
 require 'database.php';
 $post_id = $_POST['post_id'];
+
+
+$stmt = $mysqli->prepare("delete from comments where comment_id='$post_id'");
+if (!$stmt) {
+    printf("Query Prep Failed: %s\n", $mysqli->error);
+    exit;
+}
+
+$stmt->execute();
+$stmt->close();
+
 $stmt = $mysqli->prepare("delete from posts where post_id='$post_id'");
 if (!$stmt) {
     printf("Query Prep Failed: %s\n", $mysqli->error);
@@ -9,7 +20,6 @@ if (!$stmt) {
 }
 
 $stmt->execute();
-$stmt->bind_param("i", $post_id);
 $stmt->close();
 header("Location: main.php");
 ?>
