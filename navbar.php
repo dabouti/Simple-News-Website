adefa4c91f7c4fa6f6109a4a5b9836409ebf3d7f
andrewjf04 <112509381+andrewjf04@users.noreply.github.com>
2024-02-19 00:39:09 -0600

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,8 +1,6 @@
 <div class="navbar">
     <a href="main.php">Home</a>
-    <a href="createpostpage.php">Create Posts</a>
-    <a href="editpost.php">Edit/Delete Posts</a>
-    <a href="editcomment.php">Edit/Delete Comments</a>
+    <a href="createpostpage.php">Create Post</a>
     <?php
         if(!isset($_SESSION['loggedin'])) {
             echo '<a href=loginpage.php>Login</a>';
