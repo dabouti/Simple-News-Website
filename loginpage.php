6d3d2617789d9394b966c2985882272389edb135
andrewjf04 <112509381+andrewjf04@users.noreply.github.com>
2024-02-20 19:56:47 -0600

    login page styling

--- loginpage.php
+++ loginpage.php
@@ -11,36 +11,45 @@ if ($_SESSION['loggedin'] == true) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="loginpage.css">
 </head>
 
 <body>
-    <p>Login:</p>
+    <div class='formdiv'>
+    <h1>Login</h1>
     <form action='loginauth.php' method='POST'>
-        <label for='username'>Username:</label>
+        <label for='username'>Username</label>
         <input type='text' name="username" id="username" placeholder="Enter Username">
         <br>
         <br>
-        <label for='password'>Password:</label>
+        <label for='password'>Password</label>
         <input type='password' name="password" id="password" placeholder="Enter Password">
-        <button>Submit</button>
+        <div class='buttondiv'>
+        <button>Login</button>
+</div>
         <br>
         <br>
     </form>
-    <p>Sign Up:</p>
+</div>
+<div class='formdiv'>
+    <h1>Sign Up</h1>
     <form action='signupauth.php' method='POST'>
-        <label for='username2'>Username:</label>
+        <label for='username2'>Username</label>
         <input type='text' name="username" id="username2" placeholder="Enter Username">
         <br>
         <br>
-        <label for='password2'>Password:</label>
+        <label for='password2'>Password</label>
         <input type='password' name="password" id="password2" placeholder="Enter Password">
-        <button>Submit</button>
-        <br>
-        <br>
-    </form>
-
+        <div class='buttondiv'>
+        <button>Sign Up</button>
+</div>
+    </form> 
+</div>
     <form action='main.php'>
-        <button>Continue as guest</button>
+        <br>
+        <div class='buttondiv'>
+        <button>OR Continue as Guest</button>
+</div>
     </form>
 
 </body>
