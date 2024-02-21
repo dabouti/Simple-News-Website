edc05e8cee3cdf1f9f3ad4f101ab0fb158896e9b
dabouti <d.h.kim@wustl.edu>
2024-02-20 20:57:48 -0600

    added security (hashing)

--- loginauth.php
+++ loginauth.php
@@ -5,20 +5,27 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-$stmt = $mysqli->prepare("select username, password from user where username = ? and password = ?");
+$stmt = $mysqli->prepare("select COUNT(*), username, hashed_password from user where username = ?");
 if (!$stmt) {
     printf("Query Prep Failed: %s\n", $mysqli->error);
     exit;
 }
-$stmt->bind_param("ss", $username, $password);
+$stmt->bind_param("s", $username);
 $stmt->execute();
-$stmt->bind_result($user, $pass);
+$stmt->bind_result($cnt, $user, $pwd_hash);
 if ($stmt->fetch()) {
-    $_SESSION['loggedin'] = true;
-    $_SESSION['username'] = $username;
-    header("Location: main.php");
+    if ($cnt == 1 && password_verify($password, $pwd_hash)) {
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $username;
+        header("Location: main.php");
+        exit;
+    } else {
+        header("Location: loginpage.php");
+        exit;
+    }
 } else {
     header("Location: loginpage.php");
+    exit;
 }
 
 $stmt->close();
--- signupauth.php
+++ signupauth.php
@@ -2,29 +2,31 @@
 require 'database.php';
 
 $username = $_POST['username'];
-$password = $_POST['password'];
+$hashed_password = password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-$stmt = $mysqli->prepare("select username from user where username='$username'");
+
+$stmt = $mysqli->prepare("select username from user where username = ?");
 if (!$stmt) {
     printf("Query Prep Failed: %s\n", $mysqli->error);
     exit;
 }
-
+$stmt->bind_param('s', $username);
 $stmt->execute();
 $stmt->bind_result($user);
+
 if ($stmt->fetch()) {
+    $stmt->close();
     header("Location: loginpage.php");
     exit;
 }
-
 $stmt->close();
 
-$stmt = $mysqli->prepare("insert into user (username, password) values (?, ?)");
+$stmt = $mysqli->prepare("insert into user (username, hashed_password) values (?, ?)");
 if (!$stmt) {
     printf("Query Prep Failed: %s\n", $mysqli->error);
     exit;
 }
-$stmt->bind_param('ss', $username, $password);
+$stmt->bind_param('ss', $username, $hashed_password);
 $stmt->execute();
 $stmt->close();
 header("Location: loginpage.php");
